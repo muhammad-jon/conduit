90d0837a5df7596c98c0f4255a76b7511f0eb9cb
Muhammad <oqqora069005@gmail.com>
2022-03-12 09:32:10 +0500

    middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -15,7 +15,42 @@ import {
 //view => dispatch (action ) => reducer = > Store -> redux
 //view => dispatch (action ) => (middleware) = >reducer =>  Store -> middleware
 
-const localStorageMiddleware = (store) => (next) => (action) => {};
+//view => dispatch => middleware => reducer => store
+
+const promiseMiddleware = (store) => (next) => (action) => {
+  if (isPromise(action.payload)) {
+    store.dispatch({ type: ASYNC_START, subtype: action.type });
+    action.payload.then(
+      (res) => {
+        action.payload = res;
+        store.dispatch({ type: ASYNC_END, promise: action.payload });
+        store.dispatch(action);
+      },
+      (error) => {
+        console.log(error);
+        action.error = true;
+        action.payload = error.response.body;
+        store.dispatch(action);
+      }
+    );
+    return;
+  }
+  next(action);
+};
+
+const localStorageMiddleware = (store) => (next) => (action) => {
+  if (action.type === REGISTER || action.type === LOGIN) {
+    if (!action.error) {
+      window.localStorage.setItem("jwt", action.payload.user.token);
+      agent.setToken(action.payload.user.token);
+    }
+  } else if (action.type === LOGOUT) {
+    window.localStorage.setItem("jwt", "");
+    agent.setToken(null);
+  }
+
+  next(action);
+};
 
 function isPromise(v) {
   return v && typeof v.then === "function";
