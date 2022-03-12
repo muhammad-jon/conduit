e2ada19aa44805ac706cede8b7dbe5ee533044bf
Muhammad <oqqora069005@gmail.com>
2022-03-12 13:45:20 +0500

    register.js updated

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { REGISTER, UPDATE_FIELD_AUTH } from "../constants/actionTypes";
+import agent from "../agent";
+import ListErrors from "./ListErrors";
 
 const Register = () => {
+  const authState = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const changeEmail = (e) => {
+    dispatch({ type: UPDATE_FIELD_AUTH, key: "email", value: e.target.value });
+  };
+  const changePassword = (e) => {
+    dispatch({
+      type: UPDATE_FIELD_AUTH,
+      key: "password",
+      value: e.target.value,
+    });
+  };
+  const changeUsername = (e) => {
+    dispatch({
+      type: UPDATE_FIELD_AUTH,
+      key: "username",
+      value: e.target.value,
+    });
+  };
+  const submitForm = (username, email, password) => (e) => {
+    e.preventDefault();
+    dispatch({
+      type: REGISTER,
+      payload: agent.Auth.register(username, email, password),
+    });
+  };
+
   return (
     <div className="auth-page">
       <div className="container page">
@@ -12,16 +44,22 @@ const Register = () => {
               <Link to="/login">Have an account?</Link>
             </p>
 
-            <ul className="error-messages">
-              <li>That email is already taken</li>
-            </ul>
+            <ListErrors errors={authState.errors} />
 
-            <form>
+            <form
+              onSubmit={submitForm(
+                authState.username,
+                authState.email,
+                authState.password
+              )}
+            >
               <fieldset className="form-group">
                 <input
                   className="form-control form-control-lg"
                   type="text"
                   placeholder="Your Name"
+                  value={authState.username}
+                  onChange={changeUsername}
                 />
               </fieldset>
               <fieldset className="form-group">
@@ -29,6 +67,8 @@ const Register = () => {
                   className="form-control form-control-lg"
                   type="text"
                   placeholder="Email"
+                  value={authState.email}
+                  onChange={changeEmail}
                 />
               </fieldset>
               <fieldset className="form-group">
@@ -36,9 +76,15 @@ const Register = () => {
                   className="form-control form-control-lg"
                   type="password"
                   placeholder="Password"
+                  value={authState.password}
+                  onChange={changePassword}
                 />
               </fieldset>
-              <button className="btn btn-lg btn-primary pull-xs-right">
+              <button
+                className="btn btn-lg btn-primary pull-xs-right"
+                type="submit"
+                disabled={authState.inProgress}
+              >
                 Sign up
               </button>
             </form>
